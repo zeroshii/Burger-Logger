535782096af791b0e7a6fd127dee5d838f093d93
Garman_K <garnetbirthstone@hotmail.com>
2020-11-23 00:07:21 -0500

    debugging for heroku

--- models/burger.js
+++ models/burger.js
@@ -4,20 +4,20 @@ const orm = require('../config/orm');
 
 let burger = {
    
-    listBurgers: async function(){
-        let result = await orm.listBurgers();
+    listBurgers:  function(){
+        let result =  orm.listBurgers();
         console.log('In burger js...')
         return result;
     },
     
-    devourBurger: async function(id){
-        let result = await orm.devourBurger(id);
+    devourBurger:  function(id){
+        let result =  orm.devourBurger(id);
         return result;
     },
 
-    addBurger: async function(data){
+    addBurger:  function(data){
         let burgerName = data.burger_name;
-        result = await orm.addBurger(burgerName);
+        result =  orm.addBurger(burgerName);
         return result;
     }
 };
